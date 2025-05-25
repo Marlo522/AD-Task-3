34071833aae767e3fc813a1d256c542c62c5f171
Marlo522 <202311932@fit.edu.ph>
2025-05-25 09:51:02 +0800

    feat(kdramaList): expand Kdrama list with additional titles and details

--- handlers/pageData.handlers.php
+++ handlers/pageData.handlers.php
@@ -10,4 +10,97 @@ $carouselList = [
     ["src" => "/assets/img/black_knight.jpg", "alt" => "Third slide"],
 
 ];
+
+$kdramaList = [
+    [
+        "title" => "Undercover Highschool",
+        "image" => "/assets/img/undercover_highschool.jpg",
+        "release_date" => "2023-01-15",
+        "rating" => 8.2
+    ],
+    [
+        "title" => "Resident Playbook",
+        "image" => "/assets/img/resident_playbook.jpg",
+        "release_date" => "2023-03-10",
+        "rating" => 8.7
+    ],
+    [
+        "title" => "Black Knight",
+        "image" => "/assets/img/black_knight.jpg",
+        "release_date" => "2023-05-12",
+        "rating" => 7.9
+    ],
+    [
+        "title" => "Crash Landing on You",
+        "image" => "/assets/img/crash_landing_on_you.jpg",
+        "release_date" => "2019-12-14",
+        "rating" => 9.0
+    ],
+    [
+        "title" => "Itaewon Class",
+        "image" => "/assets/img/itaewon_class.jpg",
+        "release_date" => "2020-01-31",
+        "rating" => 8.5
+    ],
+    [
+        "title" => "Vincenzo",
+        "image" => "/assets/img/vincenzo.jpg",
+        "release_date" => "2021-02-20",
+        "rating" => 8.6
+    ],
+    [
+        "title" => "Start-Up",
+        "image" => "/assets/img/start_up.jpg",
+        "release_date" => "2020-10-17",
+        "rating" => 8.1
+    ],
+    [
+        "title" => "Hospital Playlist",
+        "image" => "/assets/img/hospital_playlist.jpg",
+        "release_date" => "2020-03-12",
+        "rating" => 9.1
+    ],
+    [
+        "title" => "Goblin",
+        "image" => "/assets/img/goblin.jpg",
+        "release_date" => "2016-12-02",
+        "rating" => 8.8
+    ],
+    [
+        "title" => "My Mister",
+        "image" => "/assets/img/my_mister.jpg",
+        "release_date" => "2018-03-21",
+        "rating" => 9.1
+    ],
+    [
+        "title" => "Reply 1988",
+        "image" => "/assets/img/reply_1988.jpg",
+        "release_date" => "2015-11-06",
+        "rating" => 9.2
+    ],
+    [
+        "title" => "Twenty-Five Twenty-One",
+        "image" => "/assets/img/twenty_five_twenty_one.jpg",
+        "release_date" => "2022-02-12",
+        "rating" => 8.7
+    ],
+    [
+        "title" => "Extraordinary Attorney Woo",
+        "image" => "/assets/img/attorney_woo.jpg",
+        "release_date" => "2022-06-29",
+        "rating" => 8.9
+    ],
+    [
+        "title" => "The Glory",
+        "image" => "/assets/img/the_glory.jpg",
+        "release_date" => "2022-12-30",
+        "rating" => 8.3
+    ],
+    [
+        "title" => "Kingdom",
+        "image" => "/assets/img/kingdom.jpg",
+        "release_date" => "2019-01-25",
+        "rating" => 8.4
+    ]
+];
 ?>
