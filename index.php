70f94a3ee1de3dbe3ad5defe011d9dd3687879f8
Marlo522 <202311932@fit.edu.ph>
2025-05-25 09:39:11 +0800

    feat(landing page): Include carousel component in index page

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once BASE_PATH . '/handlers/pageData.handlers.php';
 require_once BASE_PATH . '/components/templates/head.components.php';
+require BASE_PATH . '/components/carousel/carousel.components.php';
 
 ?>
 <!DOCTYPE html>
@@ -10,6 +11,7 @@ require_once BASE_PATH . '/components/templates/head.components.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mflix</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     
